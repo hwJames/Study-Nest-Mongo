3bb6c8555dbd6ff677b1bb1b81fcf952c766b148
KJH <kjhg5852@naver.com>
2021-03-01 20:12:21 +0900

    CRUD 작업

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -1,16 +1,22 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpStatus,
+  NotFoundException,
+  Param,
   Post,
+  Put,
   Query,
   Res,
 } from '@nestjs/common';
-import { CreateNoticeDto } from './dto/create-notice.dto';
-import { PaginationQueryDto } from './dto/pagination-query.dto';
 import { NoticeService } from './notice.service';
 
+// DTO
+import { CreateNoticeDto } from './dto/create-notice.dto';
+import { UpdateNoticeDto } from './dto/update-notice.dto';
+import { PaginationQueryDto } from './dto/pagination-query.dto';
 @Controller('api/v1/notice')
 export class NoticeController {
   constructor(private noticeService: NoticeService) {}
@@ -33,10 +39,61 @@ export class NoticeController {
         notice,
       });
     } catch (err) {
+      console.log(err);
       return res.status(HttpStatus.BAD_REQUEST).json({
         message: 'Error: notice not created!',
         status: 400,
       });
     }
+  }
+
+  @Get('/:no')
+  public async getNotice(@Res() res, @Param('no') no: number) {
+    const notice = await this.noticeService.findOne(no);
+    if (!notice) {
+      throw new NotFoundException('Notice does not exist!');
+    }
+    return res.status(HttpStatus.OK).json(notice);
+  }
+
+  @Put('/:no')
+  public async updateNotice(
+    @Res() res,
+    @Param('no') no: number,
+    @Body() updateNoticeDto: UpdateNoticeDto,
+  ) {
+    try {
+      const notice = await this.noticeService.update(no, updateNoticeDto);
+      if (!notice) {
+        throw new NotFoundException('Notice does not exist!');
+      }
+      return res.status(HttpStatus.OK).json({
+        message: 'does has been successfully updated',
+        notice,
+      });
+    } catch (err) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: 'Error: does not updated!',
+        status: 400,
+      });
+    }
+  }
+
+  @Delete('/:no')
+  public async deleteNotice(@Res() res, @Param('no') no: number) {
+    if (!no) {
+      throw new NotFoundException('Notice no does not exist');
+    }
+
+    const notice = await this.noticeService.remove(no);
+
+    if (!notice) {
+      throw new NotFoundException('Notice does not exist');
+    }
+
+    return res.status(HttpStatus.OK).json({
+      message: 'Notice has been deleted',
+      notice,
+    });
   }
 }
--- src/notice/notice.service.ts
+++ src/notice/notice.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+
+// Schemas
+import { Notice, NoticeDocument } from './schemas/notice.schema';
+
+// DTO
 import { PaginationQueryDto } from './dto/pagination-query.dto';
 import { CreateNoticeDto } from './dto/create-notice.dto';
-import { Notice, NoticeDocument } from './schemas/notice.schema';
+import { UpdateNoticeDto } from './dto/update-notice.dto';
 
 @Injectable()
 export class NoticeService {
@@ -11,6 +16,11 @@ export class NoticeService {
     @InjectModel(Notice.name)
     private readonly noticeModel: Model<NoticeDocument>,
   ) {}
+
+  public async create(createNoticeDto: CreateNoticeDto): Promise<Notice> {
+    const newNotice = await new this.noticeModel(createNoticeDto);
+    return newNotice.save();
+  }
 
   public async findAll(
     paginationQuery: PaginationQueryDto,
@@ -20,8 +30,36 @@ export class NoticeService {
     return await this.noticeModel.find().skip(offset).limit(limit).exec();
   }
 
-  public async create(createNoticeDto: CreateNoticeDto): Promise<Notice> {
-    const newNotice = await new this.noticeModel(createNoticeDto);
-    return newNotice.save();
+  public async findOne(no: number): Promise<NoticeDocument> {
+    const notice = await this.noticeModel.findById({ no: no }).exec();
+
+    if (!notice) {
+      throw new NotFoundException(`Notice #${no} not found`);
+    }
+
+    return notice;
+  }
+
+  public async update(
+    no: number,
+    UpdateNoticeDto: UpdateNoticeDto,
+  ): Promise<NoticeDocument> {
+    const existingCustomer = await this.noticeModel.findByIdAndUpdate(
+      { no: no },
+      UpdateNoticeDto,
+    );
+
+    if (!existingCustomer) {
+      throw new NotFoundException(`Notice #${no} not found`);
+    }
+
+    return existingCustomer;
+  }
+
+  public async remove(no: number): Promise<any> {
+    const deletedCustomer = await this.noticeModel.findByIdAndRemove({
+      no: no,
+    });
+    return deletedCustomer;
   }
 }
