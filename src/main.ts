3087be5dc5364417daca9246dbdda9a9002076e7
KJH <kjhg5852@naver.com>
2021-03-10 11:04:40 +0900

    HotReload 추가

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,16 @@ import { AppModule } from './app.module';
 // Config
 import configuration from './config/configuration.config';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(configuration().port);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 
 bootstrap();
